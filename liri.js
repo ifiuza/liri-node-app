f9212d8680ca1aa75122458f0f6f98faa041da29
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-07-17 17:49:15 -0600

    adding userInputs function and concert-this

--- liri.js
+++ liri.js
@@ -1,12 +1,62 @@
 require("dotenv").config();
-
+//VARS
+var request = require("request");
+var fs = require("fs");
 var keys = require("./keys.js");
-
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+//vars to capture user inputs.
+var userOption = process.argv[2]; 
+var inputParameter = process.argv[3];
 
-// Make it so liri.js can take in one of the following commands:
 
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+//Execute function
+UserInputs(userOption, inputParameter);
+
+//FUNCTIONS
+function UserInputs (userOption, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
+        break;
+    case 'spotify-this-song':
+        showSongInfo(inputParameter);
+        break;
+    case 'movie-this':
+        showMovieInfo(inputParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+    default: 
+        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
+}
+
+//Funtion for Concert Info: Bands in Town
+function showConcertInfo(inputParameter){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
+    request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var concerts = JSON.parse(body);
+        for (var i = 0; i < concerts.length; i++) {  
+            console.log("**********EVENT INFO*********");  
+            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
+            console.log(i);
+            fs.appendFileSync("log.txt", i+"\n");
+            console.log("Name of the Venue: " + concerts[i].venue.name);
+            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+            console.log("Venue Location: " +  concerts[i].venue.city);
+            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+            console.log("Date of the Event: " +  concerts[i].datetime);
+            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+            console.log("*****************************");
+            fs.appendFileSync("log.txt", "*****************************"+"\n");
+        }
+    } else{
+      console.log('Error occurred.');
+    }
+});}
+
+   
